c3d1cdd589e4501e1b52d4098cfbb018229da905
Austin Thompson <austintatet@gmail.com>
2026-06-25 11:58:38 -0700

    Update code

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+namespace Learning05
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Learning03 World!");
+        static void Main(string[] args)
+        {
+            List<Shape> shapes = new List<Shape>();
+
+            Square mySquare = new Square("Red", 5.0);
+            shapes.Add(mySquare);
+
+            Rectangle myRectangle = new Rectangle("Blue", 4.0, 6.0);
+            shapes.Add(myRectangle);
+
+            Circle myCircle = new Circle("Green", 3.0);
+            shapes.Add(myCircle);
+
+            Console.WriteLine("--- Shape Portfolio ---");
+            foreach (Shape shape in shapes)
+            {
+                string color = shape.GetColor();
+                double area = shape.GetArea();
+
+                Console.WriteLine($"The {color} shape has an area of {area:F2}.");
+            }
+        }
     }
 }
